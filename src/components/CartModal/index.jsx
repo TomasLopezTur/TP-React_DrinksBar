66b2babc6a37e5cbef8858bfa0d7e2ca57822eee
TomasLopezTur <tomas.lopez.tur@gmail.com>
2023-07-12 16:35:04 -0300

    terminando funcionalidad y preparando vistas

---
+++ src/components/CartModal/index.jsx
@@ -0,0 +1,32 @@
+import styles from './CartModal.module.css';
+function CartModal (){
+    return (
+        <div className={styles.modalBg}>
+            <div className={styles.modal}>
+                <i>X</i>
+                <h2>Mi Carrito</h2>
+                <section className={styles.modalBody}>
+                    <div className={styles.modalDrinksListContainer}>
+                        <article>
+                            <img src="" />
+                            <span>Nombre P</span>
+                            <span>Precio</span>
+                            <div className={styles.counter}>
+                                <button>
+                                    -
+                                </button>
+                                <span>Cantidad</span>
+                                <button>
+                                    +
+                                </button>
+                            </div>
+                        </article>
+                    </div>
+                    <aside>
+
+                    </aside>
+                </section>
+            </div>
+        </div>
+    )
+}
--- src/context/CartProvider.jsx
+++ src/context/CartProvider.jsx
@@ -1,50 +1,25 @@
 import { createContext, useReducer } from 'react';
 import PropTypes from 'prop-types';
+import { actionTypes } from '../actions/cart.actions';
+import { CartInitialState, cartReducer } from '../reducers/cart.reducer';
 
 const CartContext = createContext();
 
-const CartInitialState = {
+function CartProvider({ children }) {
+    const [state, dispatch] = useReducer(cartReducer, CartInitialState)
 
-    cartItems: [],
-
-}
-
-const actionTypes = {
-    ADD_TO_CART: 'ADD_TO_CART',
-    REMOVE_ONE_FROM_CART: 'REMOVE_ONE_FROM_CART',
-    REMOVE_ALL_FROM_CART: 'REMOVE_ALL_FROM_CART',
-    CLEAR_CART: 'CLEAR_CART',
-}
-
-function cartReducer ( state, action ){
-    const payload = action.payload;
-    switch(action.type){
-        case actionTypes.ADD_TO_CART:
-            return {
-                ...state,
-                cartItems: [...state.cartItems, payload],
-            }
-            
-
+    function addToCart(drink) {
+        dispatch({ type: actionTypes.ADD_TO_CART, payload: drink })
     }
 
-}
-
-function CartProvider ({ children }) {
-    const [state, dispatch] = useReducer(cartReducer, CartInitialState )
-
-    function addToCart ( drink ) {
-        dispatch({type: actionTypes.ADD_TO_CART, payload: drink})
+    function removeOneFromCart() {
+        dispatch({type: actionTypes.REMOVE_ONE_FROM_CART, payload: {idDrink}})
     }
-
-    function removeOneFromCart (){
-
+    function removeAllFromCart() {
+        dispatch({type: actionTypes.REMOVE_ALL_FROM_CART, payload: {idDrink}})
     }
-    function removeAllFromCart (){
-
-    }
-    function clearCart (){
-
+    function clearCart() {
+        dispatch({type: actionTypes.CLEAR_CART })
     }
     const cartValues = {
         cart: state,
@@ -66,4 +41,4 @@ CartProvider.propTypes = {
     children: PropTypes.node.isRequired,
 }
 
-export  {CartContext, CartProvider};
+export { CartContext, CartProvider };
